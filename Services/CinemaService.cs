98fbdda1965de60dd55e8271ddc9d58ff5585c56
roumen14 <roumen14@yandex.ru>
2021-01-06 21:35:20 +0300

    В класс CinemaService возвоащен UOW для соответствия остальным сервисам.

--- Services/CinemaService.cs
+++ Services/CinemaService.cs
@@ -7,28 +7,29 @@ namespace Application
 {
     public class CinemaService : ICinemaService
     {
-        private readonly ICinemaRepository _cinemaRepository;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public CinemaService(ICinemaRepository cinemaRepository) => _cinemaRepository = cinemaRepository;
+        public CinemaService(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
 
         public async Task DeleteAsync(int id)
         {
-            var cinema = await _cinemaRepository.FindByIdAsync(id);
+            var cinema = await _unitOfWork.Cinemas.FindByIdAsync(id);
             if (cinema == null)
             {
                 throw new BusinessRuleValidationException($"Объекта с идентификатором {id} не существует.");
             }
-            _cinemaRepository.Delete(cinema);
+            _unitOfWork.Cinemas.Delete(cinema);
+            await _unitOfWork.SaveAsync();
         }
 
         public async Task<List<Cinema>> GetAllAsync()
         {
-            return await _cinemaRepository.GetAllAsync();
+            return await _unitOfWork.Cinemas.GetAllAsync();
         }
 
         public async Task<Cinema> GetByIdAsync(int id)
         {
-            var res = await _cinemaRepository.FindByIdAsync(id);
+            var res = await _unitOfWork.Cinemas.FindByIdAsync(id);
             return res;
         }
     }
