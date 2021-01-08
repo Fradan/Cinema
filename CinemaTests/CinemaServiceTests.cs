ddb97eef6f8a6281e498a9184348856ee22465e9
roumen14 <roumen14@yandex.ru>
2021-01-08 17:06:46 +0300

    Добавлены тесты для сервиса кинотеатров.

---
+++ CinemaTests/CinemaServiceTests.cs
@@ -0,0 +1,87 @@
+﻿using Application;
+using AutoFixture;
+using Core;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CinemaTests
+{
+    public class CinemaServiceTests
+    {
+        [Fact]
+        public async Task GetAllCinemas_ExpectedBehaviour()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var expectedCinemas = fixture.Create<List<Cinema>>();
+
+            var cinemaRepoMock = new Mock<ICinemaRepository>();
+            cinemaRepoMock
+                .Setup((r) => r.GetAllAsync())
+                .ReturnsAsync(() => expectedCinemas);
+
+            var uowMock = new Mock<IUnitOfWork>();
+            uowMock
+                .Setup(x => x.Cinemas)
+                .Returns(cinemaRepoMock.Object);
+
+            var cinemaService = new CinemaService(uowMock.Object);
+
+            // Act
+            var actualCinemas = await cinemaService.GetAllAsync();
+
+            // Assert
+            Assert.Equal(actualCinemas, expectedCinemas);
+        }
+
+        [Fact]
+        public async Task GetAllCinemasAsync_EmptyResult()
+        {
+            // Arrange
+            var cinemaRepoMock = new Mock<ICinemaRepository>();
+            cinemaRepoMock
+                .Setup((r) => r.GetAllAsync())
+                .ReturnsAsync(() => new List<Cinema>());
+
+            var uowMock = new Mock<IUnitOfWork>();
+            uowMock
+                .Setup(x => x.Cinemas)
+                .Returns(cinemaRepoMock.Object);
+
+            var cinemaService = new CinemaService(uowMock.Object);
+
+            // Act
+            var cinemas = await cinemaService.GetAllAsync();
+
+            // Assert
+            Assert.Empty(cinemas);
+        }
+
+        [Fact]
+        public async Task GetCinemaAsync_ById_NullResult()
+        {
+            // Arrange
+            var fixture = new Fixture();
+
+            var cinemaRepoMock = new Mock<ICinemaRepository>();
+            cinemaRepoMock
+                .Setup((r) => r.FindByIdAsync(It.IsAny<int>()));
+
+            var uowMock = new Mock<IUnitOfWork>();
+            uowMock
+                .Setup(x => x.Cinemas)
+                .Returns(cinemaRepoMock.Object);
+
+            var cinemaService = new CinemaService(uowMock.Object);
+            var cinemaId = fixture.Create<int>();
+
+            // Act
+            var cinema = await cinemaService.GetByIdAsync(cinemaId);
+
+            // Assert
+            Assert.Null(cinema);
+        }
+    }
+}
---
+++ CinemaTests/SessionServiceTests.cs
@@ -0,0 +1,211 @@
+﻿using Application;
+using Application.Exceptions;
+using AutoFixture;
+using Core;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CinemaTests
+{
+    public class SessionServiceTests
+    {
+        [Fact]
+        public async Task DeleteSessionAsync_ById_ExceptionResult()
+        {
+            // Arrange
+            var fixture = new Fixture();
+
+            var sessionRepoMock = new Mock<ISessionRepository>();
+            sessionRepoMock
+                .Setup((r) => r.FindByIdAsync(It.IsAny<int>()));
+
+            var uowMock = new Mock<IUnitOfWork>();
+            uowMock
+                .Setup(x => x.Sessions)
+                .Returns(sessionRepoMock.Object);
+
+            var sessionService = new SessionService(uowMock.Object);
+            int sessionIdToDelete = fixture.Create<int>();
+
+            // Act
+            Func<Task> deleteSessionFunc = () => sessionService.DeleteSessionAsync(sessionIdToDelete);
+
+            // Assert
+            await Assert.ThrowsAsync<BusinessRuleValidationException>(deleteSessionFunc);
+        }
+
+        [Fact]
+        public async Task DeleteSessionAsync_ById_ExpectedBehaviour()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var sessionToDelete = fixture.Create<Session>();
+
+            var sessionRepoMock = new Mock<ISessionRepository>();
+            sessionRepoMock
+                .Setup((r) => r.FindByIdAsync(sessionToDelete.Id))
+                .ReturnsAsync(sessionToDelete);
+
+            var uowMock = new Mock<IUnitOfWork>();
+            uowMock
+                .Setup(x => x.Sessions)
+                .Returns(sessionRepoMock.Object);
+
+            var sessionService = new SessionService(uowMock.Object);
+
+            // Act
+            await sessionService.DeleteSessionAsync(sessionToDelete.Id);
+
+            // Assert 
+            sessionRepoMock.Verify(r => r.Delete(sessionToDelete), Times.Once);
+        }
+
+        [Fact]
+        public async Task AddSessionAsync_AlreadyExistResult()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var sessionToAdd = fixture.Create<Session>();
+
+            var sessionRepoMock = new Mock<ISessionRepository>();
+            sessionRepoMock
+                .Setup((r) => r.FindSessionByParametersAsync(sessionToAdd.SessionTime, sessionToAdd.CinemaId, sessionToAdd.FilmId))
+                .ReturnsAsync(fixture.Create<List<Session>>());
+
+            var uowMock = new Mock<IUnitOfWork>();
+            uowMock
+                .Setup(x => x.Sessions)
+                .Returns(sessionRepoMock.Object);
+
+            var sessionService = new SessionService(uowMock.Object);
+
+            // Act
+            Func<Task> addSessionFunc = () => sessionService.AddSessionAsync(sessionToAdd);
+
+            // Assert
+            await Assert.ThrowsAsync<BusinessRuleValidationException>(addSessionFunc);
+        }
+
+        [Fact]
+        public async Task AddSessionAsync_ById_SuccessResult()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var sessionToAdd = fixture.Create<Session>();
+
+            var sessionRepoMock = new Mock<ISessionRepository>();
+            sessionRepoMock
+                .Setup((r) => r.FindSessionByParametersAsync(sessionToAdd.SessionTime, sessionToAdd.CinemaId, sessionToAdd.FilmId));
+
+            var uowMock = new Mock<IUnitOfWork>();
+            uowMock
+                .Setup(x => x.Sessions)
+                .Returns(sessionRepoMock.Object);
+
+            var sessionService = new SessionService(uowMock.Object);
+            int sessionIdToDelete = fixture.Create<int>();
+
+            // Act
+            await sessionService.AddSessionAsync(sessionToAdd);
+
+            // Assert 
+            sessionRepoMock.Verify(r => r.AddAsync(sessionToAdd), Times.Once);
+        }
+
+        [Fact]
+        public async Task UpdateSessionAsync_SessionNotFound()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var sessionToAdd = fixture.Create<Session>();
+
+            var sessionRepoMock = new Mock<ISessionRepository>();
+            sessionRepoMock
+                .Setup((r) => r.FindByIdAsync(sessionToAdd.Id));
+                
+
+            var uowMock = new Mock<IUnitOfWork>();
+            uowMock
+                .Setup(x => x.Sessions)
+                .Returns(sessionRepoMock.Object);
+
+            var sessionService = new SessionService(uowMock.Object);
+            int sessionIdToDelete = fixture.Create<int>();
+
+            // Act
+            Func<Task> addSessionFunc = () => sessionService.UpdateSessionAsync(sessionToAdd.Id, sessionToAdd);
+
+            // Assert
+            await Assert.ThrowsAsync<BusinessRuleValidationException>(addSessionFunc);
+        }
+
+        [Fact]
+        public async Task UpdateSessionAsync_Success()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            
+            var curSession = fixture.Create<Session>();
+
+            var newSession = new Session
+            {
+                Id = curSession.Id,
+                CinemaId = fixture.Create<int>(),
+                FilmId = fixture.Create<int>(),
+                SessionTime = fixture.Create<DateTime>()
+            };
+
+            var sessionRepoMock = new Mock<ISessionRepository>();
+            sessionRepoMock
+                .Setup((r) => r.FindByIdAsync(newSession.Id))
+                .ReturnsAsync(curSession);
+
+            var uowMock = new Mock<IUnitOfWork>();
+            uowMock
+                .Setup(x => x.Sessions)
+                .Returns(sessionRepoMock.Object);
+
+            var sessionService = new SessionService(uowMock.Object);
+            int sessionIdToDelete = fixture.Create<int>();
+
+            // Act
+            await sessionService.UpdateSessionAsync(newSession.Id, newSession);
+
+            // Assert 
+            sessionRepoMock.Verify(r 
+                => r.Update(It.Is<Session>(x => 
+                x.Id == curSession.Id && 
+                x.CinemaId == newSession.CinemaId && 
+                x.FilmId == newSession.FilmId && 
+                x.SessionTime == newSession.SessionTime)), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetSessionAsync_ById_NullResult()
+        {
+            // Arrange
+            var fixture = new Fixture();
+
+            var sessionRepoMock = new Mock<ISessionRepository>();
+            sessionRepoMock
+                .Setup((r) => r.FindByIdAsync(It.IsAny<int>()));
+
+            var uowMock = new Mock<IUnitOfWork>();
+            uowMock
+                .Setup(x => x.Sessions)
+                .Returns(sessionRepoMock.Object);
+
+            var sessionService = new SessionService(uowMock.Object);
+            var sessionId = fixture.Create<int>();
+
+            // Act
+            var session = await sessionService.GetByIdAsync(sessionId);
+
+            // Assert
+            Assert.Null(session);
+        }
+    }
+}
